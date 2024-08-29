0d19a0f7e12067ce028a57b27d15ba52f4dbc9c7
ikizoglu <ikizoglukemal@gmail.com>
2024-08-29 14:56:27 +0300

    added index and style files

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *   INDEX TEMPLATE
+ *
+ *   Kemal İKİZOĞLU
+ *   WP-Inspire Blog
+ */
+
+get_header();
+
+?>
+
+	<?php get_template_part( 'layouts/cover-blogtitle' ); ?>
+	<?php get_template_part( 'layouts/postlist' ); ?>
+	<?php get_template_part( 'layouts/footer-main' ); ?>
+
+<?php
+
+get_footer();
